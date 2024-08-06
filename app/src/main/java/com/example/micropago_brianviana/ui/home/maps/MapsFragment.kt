40c086d51064cf9bf56df2e771bb83e1bc519e9b
BrianVianaC7 <brian.vianac@outlook.com>
2024-08-06 12:18:00 -0600

    complete map module -> add marker to current location and and toast to see latitude and longitude

--- app/src/main/java/com/example/micropago_brianviana/ui/home/maps/MapsFragment.kt
+++ app/src/main/java/com/example/micropago_brianviana/ui/home/maps/MapsFragment.kt
@@ -1,14 +1,22 @@
 package com.example.micropago_brianviana.ui.home.maps
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment.STYLE_NO_TITLE
 import androidx.fragment.app.FragmentManager
 import com.example.micropago_brianviana.R
 import com.example.micropago_brianviana.databinding.FragmentMapsBinding
+import com.example.micropago_brianviana.ui.home.HomeActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +27,7 @@ class MapsFragment : Fragment() {
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
     private lateinit var map: GoogleMap
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,12 +43,66 @@ class MapsFragment : Fragment() {
             childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync { googleMap ->
             map = googleMap
-            //createMarker()
+            enableLocation()
         }
     }
 
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation() {
+        if (!::map.isInitialized) return
+        if (isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(
+                requireContext(),
+                "Ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            map.isMyLocationEnabled = true
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Para activar la localización ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
     private fun createMarker() {
         TODO("Not yet implemented")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!::map.isInitialized) return
+        if (!isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = false
+            Toast.makeText(
+                requireContext(),
+                "Para activar la localización ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onCreateView(
